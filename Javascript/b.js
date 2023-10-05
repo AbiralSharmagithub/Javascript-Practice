6a274c58fb91328d17fad3ec086fa7ca684263ee
Abiral Sharma <abiralsharma31@gmail.com>
2023-10-05 16:44:43 +0545

    refactor:Learn about filter

---
+++ Javascript/b.js
@@ -0,0 +1,7 @@
+//filter function takes elements from original array and check the condition of the function and store the result
+//in new array for an example
+const aashish = ["chhucho","badmash","naramro"];
+const avash = aashish.filter((prabin)=> {
+return (prabin==="chhucho"|| prabin==="naramro");
+})
+console.log(avash);
