922045a71685c8dd709f2ab269b1d5348b8756e7
Abiral Sharma <abiralsharma31@gmail.com>
2023-10-09 13:56:07 +0545

    refactor:learn child nodes

--- Javascript/f.js
+++ Javascript/f.js
@@ -3,12 +3,14 @@
 // a=Number.parseInt(a);
 // alert("Type of input" +  (typeof a));
 // document.write(a);
-let write=confirm("Do you want to write");
-if (write){
-    const a=prompt("Enter the value ");
-    document.write("According to your numlber"+a+"."+"Your value is this");
-}
-else{
-    alert("You should write to get your result");
+// let write=confirm("Do you want to write");
+// if (write){
+//     const a=prompt("Enter the value ");
+//     document.write("According to your numlber"+a+"."+"Your value is this");
+// }
+// else{
+//     alert("You should write to get your result");
 
-}
+// }
+console.log(window);
+console.log(document);
