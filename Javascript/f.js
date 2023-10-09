6305a2c91d9d07b0772616c258476da5ac20c5fa
Abiral Sharma <abiralsharma31@gmail.com>
2023-10-09 12:37:49 +0545

    refactor:learn about prompt,alert and confirm

--- Javascript/f.js
+++ Javascript/f.js
@@ -1,1 +1,14 @@
+//  alert("Enter the value of a");
+// let a=prompt("Enter the value of a");
+// a=Number.parseInt(a);
+// alert("Type of input" +  (typeof a));
+// document.write(a);
+let write=confirm("Do you want to write");
+if (write){
+    const a=prompt("Enter the value ");
+    document.write("According to your numlber"+a+"."+"Your value is this");
+}
+else{
+    alert("You should write to get your result");
 
+}
--- Javascript/g.js
+++ Javascript/g.js
@@ -1,9 +1,7 @@
 const rightChevron = document.getElementById("right-chevron");
 const navBar = document.querySelector(".nav-bar");
-const nav = document.querySelectorAll(".nav");
 rightChevron.addEventListener("click", () => {
   navBar.classList.toggle("show-nav");
-  
 });
 
 
