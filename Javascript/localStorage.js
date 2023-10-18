372dd090f507954d3b5d0d316375f34eea4c3eba
Abiral Sharma <abiralsharma31@gmail.com>
2023-10-18 11:32:10 +0545

    refactor:learn local storage and session storage

---
+++ Javascript/localStorage.js
@@ -0,0 +1,16 @@
+//local storage and session storage
+// let key=prompt("enter the key");
+// let value=prompt("enter the value");
+// localStorage.setItem(key,value);
+// console.log(`The value of ${key} is ${localStorage.getItem(key)}`);
+// if(key==="red" || key==="blue")
+// {
+// localStorage.removeItem(key);
+// }
+// if(key == 0) {
+//     localStorage.clear()
+// }
+//Session storage
+window.onstorage=()=>{
+    alert("let's change the value");
+}
