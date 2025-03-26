dc0ceaacbe5f1ece7bfcbb6f0e4802ab57b01e5c
Abiral Sharma <abiralsharma31@gmail.com>
2025-03-26 21:52:33 +0545

    refactor:learn the use of paramter

--- Javascript/a.js
+++ Javascript/a.js
@@ -8,5 +8,22 @@ function pickComputermove() {
   } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
     computerMove = "sicssor";
   }
-  return computerMove;
+}
+function playGame(playermove) {
+  {
+    const computerMove = pickComputermove();
+    let conclusion = "";
+    if (computerMove === playermove) {
+      conclusion = "Tied";
+    } else if (computerMove === playermove) {
+      conclusion = "lost";
+    } else if (computerMove === playermove) {
+      conclusion = "won";
+    } else {
+      alert("You picked a wrong choice");
+    }
+    alert(
+      `You picked ${playermove}. Computer picked ${computerMove}.You ${conclusion}`
+    );
+  }
 }
