1acbc02c832c7061c9e236257a7ad1354c7fc397
Abiral Sharma <abiralsharma31@gmail.com>
2025-07-11 10:37:29 +0545

    refactor:Learn advanced function

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,11 +1,11 @@
-/* const todoList = [];
- function addtodo() {
-   const inputElement = document.querySelector(".js-name-input");
-   const name = inputElement.value;
-   todoList.push(name);
-   console.log(todoList);
-   inputElement.value = "";
- } */
+/*const todoList = [];
+function addtodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+  todoList.push(name);
+  console.log(todoList);
+  inputElement.value = "";
+}*/
 /* let i = 1;
 while (i <= 10) {
   console.log(i);
@@ -45,6 +45,21 @@ for(i=0;i<numb.length;i++)
 console.log(doubleNumb);
 */
 /*const todoList = [];
+function renderTodo(){
+let todolistHtml = "";
+todoList.forEach(function (todo, i) {
+  const html = `<div>${todo.name}</div> 
+    <div>${todo.dueDate}</div>
+    <button class=
+    "delete-todo" 
+    onclick = "{todoList.splice(${i},1);
+    renderTodo();
+    }">Delete</button>`;
+  todolistHtml = todolistHtml + html;
+  console.log(todolistHtml);
+});
+document.querySelector(".js-todo-list").innerHTML = todolistHtml;
+}
 function renderTodo() {
   let todolistHtml = "";
   for (let i = 0; i < todoList.length; i++) {
@@ -70,60 +85,113 @@ function addtodo() {
   todoList.push({ name, dueDate }); //This is called shorthand poperty syntax.
   renderTodo();
   inputElement.value = "";
-} */
-//Arrays are references
-const arrays1 = [1, 2, 3, 4];
-const arrays2 = arrays1.slice(); //This will make the copies of the value of array1.
-arrays2.push(5);
-console.log(arrays1);
-console.log(arrays2);
-//Destructuring
-const [firstValue, secondValue, thirdValue] = [1, 2, 3];
-console.log(firstValue);
-console.log(secondValue);
-console.log(thirdValue);
-//More details about loop, break and continue.
-for (i = 0; i <= 10; i++) {
-  if (i % 2 === 0) {
-    continue;
-  }
-  console.log(i);
-}
-//It is also used in while loop
-let j = 1;
-while (j <= 10) {
-  if (j % 3 === 0) {
-     j++;
-    continue;
-  }
-  console.log(j);
-  j++;
-}
-function doubleArray(numb){
-const doubleNumb=[];
-for(k=0;k<numb.length;k++)
+}*/
+/*function greeting ()
 {
-  const value=numb[k];
-  doubleNumb.push(value*2);
+  console.log("Hello bro");
 }
-return (doubleNumb);
+greeting(); //This is called the calling the function or running the function.
+//function is value.we can place it on variable.
+const function1 = function congratulating ()
+{
+  console.log("congratulation");
 }
-console.log(doubleArray([10,20,30,25,35]));
-console.log(doubleArray([1,2,3,4,5,6,7,8]));
-// If we see 0 then break out this loop
-function zeroNumb(myNumb){
-  const zeroArray = [];
-   for(l=0;l<myNumb.length;l++)
-   {
-    const numbValue = myNumb[l];
-    if(numbValue===0)
-    {
-      //break;
-      return zeroArray;
-    }
-    zeroArray.push(numbValue*2);
-   }
-   return zeroArray;
+//In the above the function is placing inside the variable function1 as a value.
+console.log(function1);
+console.log(typeof function1);
+function1();//This 'll run the type of the function.
+//since it is a value ,we can save it in object.
+const object = {
+  num:2,
+  fun:function greeting()
+  {
+    console.log("hello bro");
+  }
 }
-console.log(zeroNumb([1,2,3,4,0,5,6,7]));
-console.log(zeroNumb([4,6,9,0,3,7]));
+object.fun();
+//passing a value into a function
+function display(param)
+{
+  console.log(param);
+}
+display(2);
+function shows(intoFunction)
+{
+  intoFunction();
+}
+shows(function()
+{
+  console.log("Let's pass a function into function");
+});
+setTimeout(()=>{
+  console.log("hello bro");
+},5000);
+console.log("bye bro");*/
+// ["make dinner",
+//   "make breakfast",
+//   "make lunch"
+// ].forEach(function(value)
+// {
+//   console.log(value);
+// });
+/*["Make my life",
+  "Be my wife",
+  "Live your entire life with me"
+].forEach((value,index)=>{
+  if (value=== "Be my wife")
+  {
+    return;
+  }
+  console.log(value);
+  console.log(index);
+});
+//Arrow function
+// const arrowFunction = (value,myValue)=>{
+//   console.log(value);
+//   console.log(myValue);
+// }
+//arrowFunction("1st parameter","Second parameter");
+//Arrow function having one paramter does not have to put inside a bracket.
+const oneParam = value =>{
+  console.log(value);
+}
+oneParam("This arrow funtion only have one parameter");
+//One line arrow function
+
+const oneLine = ()=>{
+  return 2+3;
+}
+console.log(oneLine());
+//Since the function have only one line so we can write it in following ways.
+const twoLine = () =>2+3;
+console.log(twoLine());
+const arrowFunction=()=>{
+  console.log("This is arrow function");
+}
+console.log(arrowFunction());
+*/
+/*const buttonElement = document.querySelector(".js-button");
+const clickFunction = ()=>console.log("click");
+buttonElement.addEventListener('click',()=>console.log("clickFunction"));
+buttonElement.addEventListener('click',clickFunction);
+buttonElement.removeEventListener('click',clickFunction);*/
+const positiveValue=[1, -3, 5].filter((value, index) => {
+  if (value >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+});
+console.log(positiveValue);
+const greatNumber = [1,2,3,4,5,6,7,8,9].filter((value,index)=>{
+  return value>4;
+})
+console.log(greatNumber);
+const newArray = [1,2,3].map((value,index)=>{
+  return value;
+});
+console.log(newArray);
+const anotherArray = [1,2,3].map((value,index)=>{
+  return value+10;
+})
+console.log(anotherArray);
